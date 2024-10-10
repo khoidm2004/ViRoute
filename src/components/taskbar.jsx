4345df5fa4a74fa2a661f38f3afb3c7b45473475
tran.thang <tranthang.am1@gmail.com>
2024-10-10 23:49:45 +0300

    Taskbar left

--- src/components/taskbar.jsx
+++ src/components/taskbar.jsx
@@ -6,13 +6,26 @@ const Taskbar = () => {
     const handleClick = (item) => {}
   return (
     <div className="taskbar">
-      <img className="logo" src="./images/ViRoute.png"/>
+      <img className="logo" src="./images/ViRoute_white.png"/>
       <div className='taskbar-item'>
-        <Icon icon="ion:home" className='icon'/>
+        <Icon icon="subway:home-1" className='icon' />
         <span className='icon-text'>Home</span>
       </div>
       <div className='taskbar-item'>
-        <Icon icon="" className='icon'/>
+        <Icon icon="mdi:sign-direction-plus" className='icon'/>
+        <span className='icon-text'>Tracking</span>
+      </div>
+      <div className='taskbar-item'>
+        <Icon icon="heroicons:ticket-solid" className='icon'/>
+        <span className='icon-text'>Tickets</span>
+      </div>
+      <div className='taskbar-item'>
+        <Icon icon="majesticons:phone" className='icon'/>
+        <span className='icon-text'>Feedback</span>
+      </div>
+      <div className='taskbar-item'>
+        <Icon icon="icon-park-outline:change" className='icon'/>
+        <span className='icon-text'>Change city</span>
       </div>
     </div>
   );
