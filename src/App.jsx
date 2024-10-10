7a111e7becce5ea36809110b90afbc21fea7782d
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2024-10-10 20:31:02 +0300

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Taskbar from './components/taskbar'
 function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <>
-      <Taskbar />
-    </>
-  )
+  const [count, setCount] = useState(0)
+  return (
+    <>
+     <Taskbar />
+    </>
+  )
 }
 export default App
--- src/components/taskbar.jsx
+++ src/components/taskbar.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import './Taskbar.css'; // Add styling as needed
 import { Icon } from '@iconify/react';
 const Taskbar = () => {
-   const [activeItem, setActiveItem] = useState(null);
-   const handleClick = (item) => {}
+    const [activeItem, setActiveItem] = useState(null);
+    const handleClick = (item) => {}
   return (
     <div className="taskbar">
       <img className="logo" src="./images/ViRoute.png"/>
@@ -15,6 +15,6 @@ const Taskbar = () => {
         <Icon icon="" className='icon'/>
       </div>
     </div>
-    );
+  );
 };
 export default Taskbar;
