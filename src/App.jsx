907e6457222a036b2eabb45e5e84f573c7f08c92
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2024-10-10 20:04:48 +0300

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import Taskbar from './components/taskbar'
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <Taskbar />
+    </>
+  )
+}
+export default App
---
+++ src/components/taskbar.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './Taskbar.css'; // Add styling as needed
+import { Icon } from '@iconify/react';
+const Taskbar = () => {
+   const [activeItem, setActiveItem] = useState(null);
+   const handleClick = (item) => {}
+  return (
+    <div className="taskbar">
+      <img className="logo" src="./images/ViRoute.png"/>
+      <div className='taskbar-item'>
+        <Icon icon="ion:home" className='icon'/>
+        <span className='icon-text'>Home</span>
+      </div>
+      <div className='taskbar-item'>
+        <Icon icon="" className='icon'/>
+      </div>
+    </div>
+    );
+};
+export default Taskbar;
