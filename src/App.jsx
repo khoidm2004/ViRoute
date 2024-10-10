276b953bb96ac0720c100a2f8f30e1eaa49aa3b7
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2024-10-10 22:37:21 +0300

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import Taskbar from './components/taskbar'
+import { useState } from 'react';
+import './App.css';
+import Taskbar from './components/taskbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <>
-     <Taskbar />
-    </>
+    <Router>
+      <Taskbar />
+      <Routes>
+        <Route path="/login" element={<div>Login page</div>} />
+      </Routes>
+    </Router>
   )
 }
 export default App
--- src/components/taskbar.jsx
+++ src/components/taskbar.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import './Taskbar.css'; // Add styling as needed
 import { Icon } from '@iconify/react';
+import {useNavigate} from 'react-router-dom';
 const Taskbar = () => {
+    const navigate = useNavigate();
+    const loginClick = () => {
+      navigate('/login');
+    };
     const [activeItem, setActiveItem] = useState(null);
-    const handleClick = (item) => {}
+    const itemClick = (item) => {}
   return (
     <div className="taskbar">
       <img className="logo" src="./images/ViRoute.png"/>
@@ -14,13 +19,10 @@ const Taskbar = () => {
       <div className='taskbar-item'>
         <Icon icon="" className='icon'/>
       </div>
-      <div className='taskbar-item'>
-        <span className='login-reg'>
-          <tr>
-            <td>
-              <Icon icon="material-symbols:account-circle" className='login-icon'/>
-            </td>
-          </tr>
+      <div className='taskbar-item login-reg' onClick={loginClick}>
+        <span className='login-content'>
+          <Icon icon="material-symbols:account-circle" className='login-icon'/>
+          <span className='login-text'>Login/Sign up</span>
         </span>
       </div>
     </div>
