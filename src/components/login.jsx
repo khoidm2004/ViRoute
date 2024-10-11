dba4d659f8217520cc3e87aaaa1fbe410c063c9b
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2024-10-11 15:19:27 +0300

    Committed

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,9 +4,13 @@ import './login.css';
 const Login = () => {
   return (
     <div className='login-page'>
-      <input type="text" className='input-field' placeholder='Username' />
+      <form className='login-container'> {/*action='' to send data*/}
+        <label className='text'>Email</label>
+        <input type="text" className='input-field' placeholder='Password'/>
+        <label className='text'>Password</label>
+        <input type="text" className='input-field' placeholder='Email' />
+      </form>
     </div>
   );
 };
-
 export default Login
