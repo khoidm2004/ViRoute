3b063a5e2178be107a0067760087f474bfb9cf08
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2024-10-30 21:35:18 +0200

    Committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import './App.css';
-import Footer from './components/footer/footer';
-import Taskbar from './components/taskbar/taskbar';
-import Login from './components/login/login';
-import Register from './components/register/register';
-import Homescreen from './components/home/homescreen';
-import RouteDetail from './components/home/routedetail';
-import Map from './components/map';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import AppRoutes from './Routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  /*path /hn */
   return (
     <Router>
-      <Routes>
-        <Route path="/:cityCode/" element={<><Taskbar/><Homescreen/></>} />
-        <Route path="/:cityCode/:route" element={<><Taskbar/><RouteDetail /></>} />
-        <Route path="/:cityCode/tracking" element={<><Taskbar/></>}/>
-        <Route path="/:cityCode/tickets" element={<><Taskbar/></>}/>
-        <Route path="/:cityCode/feedback" element={<><Taskbar/></>}/>
-        <Route path="/login" element={<><Login/></>} />
-        <Route path="/register" element={<><Register/></>}/>       
-        <Route path="/map" element={<Map/>}/>
-      </Routes>
+        <AppRoutes/>
     </Router>
   );
 };
