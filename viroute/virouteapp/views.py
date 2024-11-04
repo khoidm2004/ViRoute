6cfb10ed53e66b53b01ef7a5815cf53ff9b11ccb
pingviini314159 <tandung270101@gmail.com>
2024-11-04 12:26:00 +0200

    adjust url correctly to makemiragation

--- viroute/virouteapp/views.py
+++ viroute/virouteapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+import requests
+from django.conf import settings
 # Login authen
 from django.core.exceptions import MultipleObjectsReturned
 from allauth.socialaccount.models import SocialAccount
@@ -8,32 +9,17 @@ from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 
-
-# Testing FROM this part - whoever delete this shit is a cunt, unable to be a human. This is harsh, my backend, my rules.
-def projects(request):
-    return render(request, 'login.jsx')
-
-def project(request, pk):
-    return HttpResponse('Page : ' + pk)
-# Testing TO this part - whoever delete this shit is a cunt, unable to be a human. This is harsh, my backend, my rules.
-
-
-# START READING FROM HERE
-import requests
-from django.conf import settings
-
 #Get route/ map API
 def get_route(request):
     start = request.GET.get('start')
     end = request.GET.get('end')
-    url = f"https://api.openrouteservice.org/v2/directions/driving-car?
-            api_key=5b3ce3597851110001cf62481c184721ac24419cbc62a1f87c43d9dc&"
+    url = f"https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf62481c184721ac24419cbc62a1f87c43d9dc&start={start}&end={end}"
 # syntax: &start = {start} & end = {end}
 # test location: &start=105.883999,21.049659&end=105.855546,21.024705
     respond = requests.get(url) # Request API from this
 
     if respond.status_code == 200:
-        return JsonResponse(respond.json())
+        return JsonResponse(respond.json()) #Return json data
     else:
         return JsonResponse({'error': respond.status_code})
 
@@ -54,7 +40,7 @@ class GitHubLogin(SocialLoginView):
             account_list = [{"id": account.id, "username": account.username} for account in accounts]
 
             return JsonResponse({
-                'error': 'Nhiều tài khoản GitHub đã được liên kết với tài khoản này.',
+                'error': 'Many Github accounts have been linked.',
                 'accounts': account_list,  # Return accounts list
             }, status=400)
 
