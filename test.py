5f98b8a20ff8153fd7aebb759225827382086683
pingviini314159 <tandung270101@gmail.com>
2024-11-12 15:49:19 +0200

    add  example for signup template

--- test.py
+++ test.py
@@ -2,10 +2,22 @@ import requests
 
 api_url = "http://127.0.0.1:8000/api/login/"  
 
+#example for sign in
 data = {
     "userEmail": "destinyblack@example.com",
     "password": "Z^01IbCjS)"
 }
+
+#example for sign up
+'''
+{
+    "userEmail": "thongngunhuconlon@gmail.com",
+    "fullName": "Thong",
+    "phoneNumber": "+84961697918",
+    "password":"conmevailonthat"
+    }
+'''
+
 
 response = requests.post(api_url, json=data)
 
