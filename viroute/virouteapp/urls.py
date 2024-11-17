91d37eeff7a3ac9db709c78ff1eb32a46b1e829c
pingviini314159 <tandung270101@gmail.com>
2024-11-17 23:18:27 +0200

    add app's urls

--- viroute/virouteapp/urls.py
+++ viroute/virouteapp/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.projects),
     path('project/<str:pk>', views.project),
     path('get_route', views.get_route, name = 'get_route'),
+    path('ticket', views.ticketList, name = 'ticket_list'),
+    path('', include('virouteapp.urls'))
 ]
