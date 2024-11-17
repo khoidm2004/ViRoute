b830b7e3705ecf439810ef4e4b8a8dd0b3f6a58a
pingviini314159 <tandung270101@gmail.com>
2024-11-17 23:17:54 +0200

    register superuser, admin

--- viroute/virouteapp/admin.py
+++ viroute/virouteapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Bus, Metro, Ticket, Account, User, UserTicket, AccountHasUser
 
-# Register your models here.
+admin.site.register(Ticket)
+
+#create superuser to access admin page
+'''
+python manage.py createsuperuser
+python manage.py runserver
+'''
