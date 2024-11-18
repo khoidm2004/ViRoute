568e993edcfe868c995041d0859c0b552bd71703
Tran Dac Thang <144015732+tranthangok@users.noreply.github.com>
2024-11-18 02:33:25 +0200

    làm not_found

---
+++ src/components/not_found/not_found.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './not_found.css';
+import Footer from '../footer/footer';
+
+function NotFound() {
+    return (
+        <>
+        <div className="not-found-container">
+            <h1 className="not-found-title">404</h1>
+            <p className="not-found-message">Oops! The page you're looking for doesn't exist.</p>
+            <a href="/" className="not-found-home-link">Go back to Home</a>
+        </div>
+        <Footer/>
+        </>
+    );
+}
+
+export default NotFound;
