77e83185a466a226a964fcc94336c916473c29d4
pingviini314159 <tandung270101@gmail.com>
2024-11-27 15:59:32 +0200

    add register that can be observeb in admin site

--- viroute/virouteapp/admin.py
+++ viroute/virouteapp/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Bus, Metro, Ticket, Account, User, UserTicket, AccountHasUser
 
 admin.site.register(Ticket)
+admin.site.register(User)
+admin.site.register(Bus)
+admin.site.register(Metro)
+admin.site.register(Account)
+admin.site.register(UserTicket)
+admin.site.register(AccountHasUser)
+
 
 #create superuser to access admin page
 '''
