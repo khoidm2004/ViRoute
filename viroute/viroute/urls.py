11263c6cceb93624a54a637b70737a9f4e4af00b
pingviini314159 <tandung270101@gmail.com>
2024-11-27 16:57:31 +0200

    add media url

--- viroute/viroute/urls.py
+++ viroute/viroute/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from virouteapp.views import UserLoginView
 from virouteapp import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -25,4 +26,8 @@ urlpatterns = [
     path('api/login/', UserLoginView.as_view(), name='login'),
     path('signup/', views.signup, name='signup'),
     path('tickets/', views.ticketList, name='tickets'),
+    path('get_image/<str:image_name>/', views.get_image_by_name, name='get_image_by_name'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
