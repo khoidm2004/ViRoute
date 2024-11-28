5d2aba875b00b362a51967055346970d7b417e01
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2024-11-28 19:40:48 +0200

    Change picture to div

--- src/services/useLogin.js
+++ src/services/useLogin.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const login = async (userEmail, password) => {
   try {
-    const { data } = await axios.post('http://127.0.0.1:8000/api/login/', { userEmail, password });
+    const { data } = await axios.post('https://test-production-b0ae.up.railway.app/api/login', { userEmail, password });
     return data.user;
   } catch (error) {
     return { success: false, message: error.response?.data?.message || 'Login failed' };
--- src/services/useRegister.js
+++ src/services/useRegister.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const useRegister = async (userData) => {
   try {
-    const response = await axios.post('http://127.0.0.1:8000/signup/', userData); 
+    const response = await axios.post('https://test-production-b0ae.up.railway.app/signup/', userData); 
     return { success: true, message: response.data.message || 'Registration successful!' };
   } catch (error) {
     return { success: false, message: error.response?.data?.message || 'Registration failed' };
