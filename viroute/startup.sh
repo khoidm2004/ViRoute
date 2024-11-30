6d3ee9baeb76488231790e39bb333a57cf823e5b
pingviini314159 <tandung270101@gmail.com>
2024-11-30 21:57:03 +0200

    hehe

--- viroute/startup.sh
+++ viroute/startup.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Activate the virtual environment
-source .venv/Scripts/activate
+# # Activate the virtual environment
+# source .venv/Scripts/activate
 
-# Start Gunicorn
-gunicorn --workers=3 viroute.wsgi:application
+# # Start Gunicorn
+# gunicorn --workers=3 viroute.wsgi:application
