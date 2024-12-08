224d4a4c425d7ca1deea029fcaae63f6132f50d1
pingviini314159 <tandung270101@gmail.com>
2024-12-08 23:16:40 +0200

    add user's feedback to google sheet

--- viroute/virouteapp/urls.py
+++ viroute/virouteapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('get_route', views.get_route, name = 'get_route'),
     path('ticket', views.ticketList, name = 'ticket_list'),
     path('', include('virouteapp.urls')),
-    path('api/bus_routes/', views.get_bus_routes, name='get_bus_routes')
+    path('api/bus_routes/', views.get_bus_routes, name='get_bus_routes'),
+    path('api/submit_feedback/', views.submit_feedback, name='submit_feedback')
 ]
