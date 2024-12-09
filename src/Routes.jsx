55f2281635a2d4db4dae7e9dfc1f18cd30eaa452
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2024-12-09 02:22:35 +0200

    test

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -27,7 +27,7 @@ function AppRoutes() {
           <Route path="/reclaimpass/success_change" element={<><Taskbar/><SuccessChange/></>}/>
 
           <Route path="/:id" element={<UserInformation_page/>} />
-          <Route path="/:bus_start-:bus_end" element={<p>Route</p>} />
+          <Route path="/route/:bus_start-:bus_end" element={<p>Route</p>} />
 
           <Route path="/*" element={<><Taskbar/><NotFound/></>}/>
 
